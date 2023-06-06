7dcec9504028bcee3a5ab5619d3f70cef4d5ebcd
MQ <mq@4mq.xyz>
2023-06-06 23:05:59 +0200

    remove whitenoise for tests

--- facility/facility/settings_test.py
+++ facility/facility/settings_test.py
@@ -6,3 +6,14 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# Remove whitenoise from middleware
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
