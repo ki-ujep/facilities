6d461536ddf43ad364680008e64a9b57035f0cb4
Priban <lukaspriban01@gmail.com>
2023-06-07 20:18:57 +0200

    category devices reverse not working

--- facility/client/tests/test_views.py
+++ facility/client/tests/test_views.py
@@ -1,6 +1,7 @@
 from django.test import TestCase, SimpleTestCase, Client
 from django.urls import reverse
-from client.models import Contact
+from client.models import Contact, Category, Device
+from django.shortcuts import get_object_or_404
 
 class AboutPageTests(SimpleTestCase):
     def __init__(self, *args, **kwargs):
@@ -96,3 +97,40 @@ class ContactsPageTests(TestCase):
         response = self.client.get(reverse("contacts", args=("name",)))
         self.assertContains(response, 'Test Contact 1')
         self.assertContains(response, 'Test Contact 2')
+
+class CategoryDevicesListViewTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        category1 = Category.objects.create(name='Test Category 1')
+        category2 = Category.objects.create(name='Test Category 2')
+
+        Device.objects.create(name='Device 1', category=category1)
+        Device.objects.create(name='Device 2', category=category1)
+        Device.objects.create(name='Device 3', category=category2)
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get(reverse("categorydevices", args=(1,"name")))
+        self.assertEqual(response.status_code, 200)
+    
+    """
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse("categorydevices", args=(1,"name")))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse("categorydevices", args=(1,"name")))
+        self.assertTemplateUsed(response, "categorydevices.html")
+
+    def test_view_displays_correct_category_devices(self):
+        response = self.client.get(reverse("categorydevices", args=(1,"name")))
+        self.assertContains(response, 'Device 1')
+        self.assertContains(response, 'Device 2')
+        self.assertNotContains(response, 'Device 3')
+
+        response = self.client.get(reverse("categorydevices", args=(2,"name")))
+        self.assertContains(response, 'Device 3')
+        self.assertNotContains(response, 'Device 1')
+        self.assertNotContains(response, 'Device 2')
+        """
+    # TODO: test for correct reverse url
