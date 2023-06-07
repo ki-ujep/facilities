98ab5676a04e844909b203bdc9e67bbe417802a9
Priban <lukaspriban01@gmail.com>
2023-06-07 18:43:21 +0200

    urls tests

--- facility/client/tests/test_urls.py
+++ facility/client/tests/test_urls.py
@@ -1,27 +1,53 @@
-from django.test import TestCase, SimpleTestCase
-from django.urls import reverse
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from client import views
 
+class UrlsTest(SimpleTestCase):
 
-class AboutPageTests(SimpleTestCase):
-    # @classmethod
-    # def setUpTestData(cls):
-    #    Student.objects.create(first_name="Peter", last_name="John", reg_number="111b2")
-    """
-    def test_url_exists_at_correct_location(self):
-        response = self.client.get("about")
-        self.assertEqual(response, 200)
-        
-    def test_url_available_by_name(self):  
-        response = self.client.get(reverse("about"))
-        self.assertEqual(response.status_code, 200)
+    def test_home_url_resolves(self):
+        url = reverse('home')
+        self.assertEquals(resolve(url).func, views.home)
 
-    def test_template_name_correct(self):  
-        response = self.client.get(reverse("about"))
-        self.assertTemplateUsed(response, "about.html")
+    def test_help_url_resolves(self):
+        url = reverse('help')
+        self.assertEquals(resolve(url).func, views.help_view)
 
-    def test_template_content(self):
-        response = self.client.get(reverse("about"))
-        self.assertContains(response, "<h2>About</h2>")
-        self.assertNotContains(response, "Under construction ...")
-    """
-    pass
+    def test_about_url_resolves(self):
+        url = reverse('about')
+        self.assertEquals(resolve(url).func, views.about)
+
+    def test_facultydevices_url_resolves(self):
+        url = reverse('facultydevices', args=[1])
+        self.assertEquals(resolve(url).func.view_class, views.FacultyDevicesListView)
+
+    def test_contactdevices_url_resolves(self):
+        url = reverse('contactdevices', args=[1, 'order'])
+        self.assertEquals(resolve(url).func.view_class, views.ContactDevicesListView)
+
+    def test_device_url_resolves(self):
+        url = reverse('device', args=[1])
+        self.assertEquals(resolve(url).func.view_class, views.DeviceDetailView)
+
+    def test_contacts_url_resolves(self):
+        url = reverse('contacts', args=['order'])
+        self.assertEquals(resolve(url).func.view_class, views.ContactsListView)
+
+    def test_search_result_url_resolves(self):
+        url = reverse('search_result')
+        self.assertEquals(resolve(url).func, views.search_result)
+
+    def test_categorydevices_url_resolves(self):
+        url = reverse('categorydevices', args=[1, 'order'])
+        self.assertEquals(resolve(url).func.view_class, views.CategoryDevicesListView)
+
+    def test_departmentdevices_url_resolves(self):
+        url = reverse('departmentdevices', args=[1, 'order'])
+        self.assertEquals(resolve(url).func.view_class, views.DepartmentDevicesListView)
+
+    def test_laboratorydevices_url_resolves(self):
+        url = reverse('laboratorydevices', args=[1, 'order'])
+        self.assertEquals(resolve(url).func.view_class, views.LaboratoryDevicesListView)
+
+    def test_usagedevices_url_resolves(self):
+        url = reverse('usagedevices', args=[1, 'order'])
+        self.assertEquals(resolve(url).func.view_class, views.UsageDevicesListView)
