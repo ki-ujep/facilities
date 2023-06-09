fdaac006386cc7eb2be07cf4553a475c9894fa7f
Priban <lukaspriban01@gmail.com>
2023-06-09 13:15:01 +0200

    more view tests

--- facility/client/tests/test_views.py
+++ facility/client/tests/test_views.py
@@ -171,3 +171,117 @@ class CategoryDevicesListViewTests(TestCase):
         response = self.client.get(reverse('categorydevices', kwargs={'category_id': self.category1.id, 'order': 'asc'}))
         devices = list(response.context['devices'])
         self.assertEqual(devices, [self.device1a, self.device1b])
+
+class DepartmentDevicesListViewTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up Faculty for the devices
+        cls.faculty = Faculty.objects.create(name='Test Faculty')
+
+        # Set up two Departments and two Devices for each department
+        cls.department1 = Department.objects.create(name='Department 1', faculty=cls.faculty)
+        cls.device1a = Device.objects.create(name='Device 1a', department=cls.department1, faculty=cls.faculty)
+        cls.device1b = Device.objects.create(name='Device 1b', department=cls.department1, faculty=cls.faculty)
+
+        cls.department2 = Department.objects.create(name='Department 2', faculty=cls.faculty)
+        cls.device2a = Device.objects.create(name='Device 2a', department=cls.department2, faculty=cls.faculty)
+        cls.device2b = Device.objects.create(name='Device 2b', department=cls.department2, faculty=cls.faculty)
+
+    def test_response_data(self):
+        response = self.client.get(reverse('departmentdevices', kwargs={'department_id': self.department1.id, 'order': 'asc'}))
+        content = str(response.content)
+
+        # Check the devices of department1 are in the response content
+        self.assertIn(self.device1a.name, content)
+        self.assertIn(self.device1b.name, content)
+
+        # Check the devices of department2 are not in the response content
+        self.assertNotIn(self.device2a.name, content)
+        self.assertNotIn(self.device2b.name, content)
+
+    def test_ordering(self):
+        response = self.client.get(reverse('departmentdevices', kwargs={'department_id': self.department1.id, 'order': 'desc'}))
+        devices = list(response.context['devices'])
+        self.assertEqual(devices, [self.device1b, self.device1a])
+
+        response = self.client.get(reverse('departmentdevices', kwargs={'department_id': self.department1.id, 'order': 'asc'}))
+        devices = list(response.context['devices'])
+        self.assertEqual(devices, [self.device1a, self.device1b])
+
+class LaboratoryDevicesListViewTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up Faculty for the devices
+        cls.faculty = Faculty.objects.create(name='Test Faculty')
+
+        # Set up two Laboratories and two Devices for each laboratory
+        cls.laboratory1 = Laboratory.objects.create(name='Laboratory 1', faculty=cls.faculty)
+        cls.device1a = Device.objects.create(name='Device 1a', laboratory=cls.laboratory1, faculty=cls.faculty)
+        cls.device1b = Device.objects.create(name='Device 1b', laboratory=cls.laboratory1, faculty=cls.faculty)
+
+        cls.laboratory2 = Laboratory.objects.create(name='Laboratory 2', faculty=cls.faculty)
+        cls.device2a = Device.objects.create(name='Device 2a', laboratory=cls.laboratory2, faculty=cls.faculty)
+        cls.device2b = Device.objects.create(name='Device 2b', laboratory=cls.laboratory2, faculty=cls.faculty)
+
+    def test_response_data(self):
+        response = self.client.get(reverse('laboratorydevices', kwargs={'laboratory_id': self.laboratory1.id, 'order': 'asc'}))
+        content = str(response.content)
+
+        # Check the devices of laboratory1 are in the response content
+        self.assertIn(self.device1a.name, content)
+        self.assertIn(self.device1b.name, content)
+
+        # Check the devices of laboratory2 are not in the response content
+        self.assertNotIn(self.device2a.name, content)
+        self.assertNotIn(self.device2b.name, content)
+
+    def test_ordering(self):
+        response = self.client.get(reverse('laboratorydevices', kwargs={'laboratory_id': self.laboratory1.id, 'order': 'desc'}))
+        devices = list(response.context['devices'])
+        self.assertEqual(devices, [self.device1b, self.device1a])
+
+        response = self.client.get(reverse('laboratorydevices', kwargs={'laboratory_id': self.laboratory1.id, 'order': 'asc'}))
+        devices = list(response.context['devices'])
+        self.assertEqual(devices, [self.device1a, self.device1b])
+
+from django.urls import reverse
+
+class UsageDevicesListViewTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up Faculty for the devices
+        cls.faculty = Faculty.objects.create(name='Test Faculty')
+
+        # Set up two Usages and two Devices for each usage
+        cls.usage1 = Usage.objects.create(academical_usage='Usage 1')
+        cls.device1a = Device.objects.create(name='Device 1a', faculty=cls.faculty)
+        cls.device1b = Device.objects.create(name='Device 1b', faculty=cls.faculty)
+        cls.device1a.usages.add(cls.usage1)
+        cls.device1b.usages.add(cls.usage1)
+
+        cls.usage2 = Usage.objects.create(academical_usage='Usage 2')
+        cls.device2a = Device.objects.create(name='Device 2a', faculty=cls.faculty)
+        cls.device2b = Device.objects.create(name='Device 2b', faculty=cls.faculty)
+        cls.device2a.usages.add(cls.usage2)
+        cls.device2b.usages.add(cls.usage2)
+
+    def test_response_data(self):
+        response = self.client.get(reverse('usagedevices', kwargs={'usage_id': self.usage1.id, 'order': 'asc'}))
+        content = str(response.content)
+
+        # Check the devices of usage1 are in the response content
+        self.assertIn(self.device1a.name, content)
+        self.assertIn(self.device1b.name, content)
+
+        # Check the devices of usage2 are not in the response content
+        self.assertNotIn(self.device2a.name, content)
+        self.assertNotIn(self.device2b.name, content)
+
+    def test_ordering(self):
+        response = self.client.get(reverse('usagedevices', kwargs={'usage_id': self.usage1.id, 'order': 'desc'}))
+        devices = list(response.context['devices'])
+        self.assertEqual(devices, [self.device1b, self.device1a])
+
+        response = self.client.get(reverse('usagedevices', kwargs={'usage_id': self.usage1.id, 'order': 'asc'}))
+        devices = list(response.context['devices'])
+        self.assertEqual(devices, [self.device1a, self.device1b])
